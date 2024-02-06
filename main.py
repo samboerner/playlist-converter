fdb8c1e9a9a18eba1d9150bf01080d657b9d51a7
Sam Boerner <samboerner3@gmail.com>
2024-02-05 22:54:06 -0600

    The 'get_playlists' function is now working.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import spotipy
 import spotipy.util as util
-from google.oauth2.credentials import Credentials
+
+'''from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
+from googleapiclient.discovery import build'''
 import os
 import json
 from dotenv import load_dotenv
@@ -14,43 +15,54 @@ SPOTIFY_CLIENT_ID = os.getenv("CLIENT_ID")
 SPOTIFY_CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 SPOTIFY_REDIRECT_URI = os.getenv("REDIRECT_URI")
 
-# Get YouTube API credentials
+username = ''
+
+try:
+    token = util.prompt_for_user_token(username, 'playlist-read-private', client_id=SPOTIFY_CLIENT_ID,
+                                        client_secret=SPOTIFY_CLIENT_SECRET,
+                                        redirect_uri=SPOTIFY_REDIRECT_URI)
+except:
+    os.remove(f".cache-{username}")
+    token = util.prompt_for_user_token(username, 'playlist-read-private', client_id=SPOTIFY_CLIENT_ID,
+                                       client_secret=SPOTIFY_CLIENT_SECRET,
+                                       redirect_uri=SPOTIFY_REDIRECT_URI)
+
+sp = spotipy.Spotify(auth=token)
+
+'''# Get YouTube API credentials
 YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
 
-print(SPOTIFY_REDIRECT_URI)
+'''
 
 def get_playlists(username):
-    # Get Spotify API token
-    token = util.prompt_for_user_token(username,'playlist-read-private',
-                                        client_id=SPOTIFY_CLIENT_ID,
-                                        client_secret=SPOTIFY_CLIENT_SECRET,
-                                        redirect_uri='http://localhost:8080/')
-    # Create Spotify API instance
-    sp = spotipy.Spotify(auth=token)
 
+    playlists = []
+    offset = 0
+    limit = 50
     # Get the user's playlists
-    playlists = sp.user_playlists(username)
+    while True:
+
+        response = sp.user_playlists(username, offset=offset, limit=limit)
+        if not response['items']:
+            break
+
+        playlists.extend(response['items'])
+        offset += limit
 
     playlist_dict = {}
 
-    for playlist in playlists['items']:
+    for playlist in playlists:
         playlist_dict[playlist['name']] = playlist['id']
+    
+    return playlist_dict
 
-playlists = get_playlists('sboerner3')
-
-for idx, name, playlist_id in playlists.items():
-    print(f"{idx}. {name}: {playlist_id}")
-
+idx = 0
+playlists = get_playlists(username)
+for name, playlist_id in playlists.items():
+    idx += 1
+    print(f"{idx}. {name}, {playlist_id}")
 
 def get_playlist_tracks(username, playlist_id):
-    # Get Spotify API token
-    token = util.prompt_for_user_token(username,'playlist-read-private',
-                                        client_id=SPOTIFY_CLIENT_ID,
-                                        client_secret=SPOTIFY_CLIENT_SECRET,
-                                        redirect_uri=SPOTIFY_REDIRECT_URI)
-    
-    # Create a Spotify API instance
-    sp = spotipy.Spotify(auth=token)
 
     # Get the user's playlist
     playlist = sp.user_playlist(username, playlist_id)
